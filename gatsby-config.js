38b79802153e4d86437806879aab6105b536f64f
Nana Yaw <nanayaw@Nanas-MacBook-Pro.local>
2020-04-10 03:37:33 +0000

    package added

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,15 @@ module.exports = {
       },
     },
     {
+      resolve: "gatsby-plugin-use-dark-mode",
+      options: {
+        classNameDark: "dark-mode",
+        classNameLight: "light-mode",
+        storageKey: "darkMode",
+        minify: true,
+      },
+    },
+    {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
         fonts: [
