e4340574a3b9925f0c7a5be4c1da8bbf1a4a6fe3
Nana Yaw <nanayaw@Nanas-MacBook-Pro.local>
2020-04-24 23:22:00 +0000

    changes made

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,18 @@
  */
 
 // You can delete this file if you're not using it
+import React from "react"
+import { ThemeProvider, ColorModeProvider, CSSReset } from "@chakra-ui/core"
+import theme from "./src/gatsby-plugin-chakra-ui/theme"
+
+import "./tailwind.css"
+import "./index.scss"
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={theme}>
+    <ColorModeProvider>
+      <CSSReset />
+      {element}
+    </ColorModeProvider>
+  </ThemeProvider>
+)
