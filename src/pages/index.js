d4e033aa4b7159e23a03960ba4f0cc1ea1a56bd1
Nana Yaw <nanayaw@Nanas-MacBook-Pro.local>
2020-04-25 13:47:56 +0000

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -108,7 +108,7 @@ const IndexPage = () => {
         <Flex wrap="wrap" justify="center" align="center" h="100%">
           <Flex
             color="white"
-            fontFamily="uses"
+            fontFamily="sans"
             fontSize="2xl"
             ref={(el) => (content = el)}
           >
